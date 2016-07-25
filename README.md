7dd0a41f6c19f3c5951b6df82216956ecec0bba9
Ryan Beck <ryanbeck@Ryans-MacBook-Pro.local>
2016-07-25 12:38:13 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# T2 is the first ever Flow Manager (FM)
+## Track the start time of each task throughout your day.
+### There are no end times, because in life we go from one task to the next. So does T2.
---
+++ taskTracker.js
@@ -0,0 +1,39 @@
+'use strict';
+let doc = require('dynamodb-doc');
+let dynamo = new doc.DynamoDB();
+const table = 'Tasks';
+const offset = -7;
+
+exports.handler = (event, context, callback) => {
+    dynamo.putItem(itemParams(event), function(err, response) {
+	    if (err) console.log(err);
+	    context.done(null,'');
+	});
+};
+
+function item(event) {
+    var message = event.Records[0].Sns.Message;
+    var date = new Date( new Date().getTime() + offset * 3600 * 1000);
+    var day = date.toDateString()
+	var time = date.toTimeString().split("G")[0];
+    var subject = event.Records[0].Sns.Subject;
+    var messageId = event.Records[0].Sns.MessageId;
+    var snsPublishTime = event.Records[0].Sns.Timestamp;
+    var startTime = date.toString().split("G")[0];
+    console.log('From SNS:', message);
+    console.log('At:', startTime);
+    return {
+        Day: day,
+	    StartTime: time,
+	    Task: message,
+	    Subject: subject,
+	    SnsMessageId: messageId
+	    }
+}
+
+function itemParams(event) {
+    return {
+        TableName: table,
+	    Item: item(event)
+	    };
+}
